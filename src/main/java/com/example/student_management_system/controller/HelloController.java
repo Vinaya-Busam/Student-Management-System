28dc6f6b2a8faa02feb6b1e6735614ecc30f4f25
Vinaya <vinayaa.busam18@gmail.com>
2026-07-19 11:15:52 +0000

    Made changes

--- src/main/java/com/example/student_management_system/controller/HelloController.java
+++ src/main/java/com/example/student_management_system/controller/HelloController.java
@@ -2,9 +2,10 @@ package com.example.student_management_system.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/student")
 public class HelloController {
 
     @GetMapping("/hello")
