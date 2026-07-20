646cfcaf1ae1fab70bbfe3c607af89f16cc7c83e
Vinaya <vinayaa.busam18@gmail.com>
2026-07-20 05:08:50 +0000

    Created APIs

--- src/main/java/com/example/student_management_system/controller/HelloController.java
+++ src/main/java/com/example/student_management_system/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.example.student_management_system.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,5 +15,20 @@ public class HelloController {
     @GetMapping("/hello")
     public String greet() {
         return "Hello World!!";
+    }
+
+    @PostMapping("/add")
+    public String add() {
+        return "Student Added";
+    }
+
+    @PutMapping("/update")
+    public String update() {
+        return "Updated";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete() {
+        return "Deleted";
     }
 }
