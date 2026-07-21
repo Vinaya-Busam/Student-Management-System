a1a87412177c472cd7aef9540d5606b5d9268ea9
Vinaya <vinayaa.busam18@gmail.com>
2026-07-21 06:10:09 +0000

    Connect to Database

--- src/main/java/com/example/student_management_system/model/Student.java
+++ src/main/java/com/example/student_management_system/model/Student.java
@@ -1,10 +1,20 @@
 package com.example.student_management_system.model;
 
+import jakarta.persistence.*;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 @Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     private String name;
     private int age;
     private String email;
--- src/main/java/com/example/student_management_system/service/StudentService.java
+++ src/main/java/com/example/student_management_system/service/StudentService.java
@@ -2,54 +2,44 @@ package com.example.student_management_system.service;
 
 import com.example.student_management_system.model.Student;
 import org.springframework.stereotype.Service;
+import com.example.student_management_system.repository.StudentRepo;
 import java.util.*;
 
 @Service
 public class StudentService {
 
-    private ArrayList<Student> students = new ArrayList<>();
+    private StudentRepo studentRepo;
+    public StudentService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
 
     public Student addStudent(Student student) {
-        students.add(student); 
-        return student;
+        return studentRepo.save(student);
     }
 
     public List<Student> getAllStudents() {
-        return students;
+        return studentRepo.findAll();
     }
 
     public Student getStudentById(int id) {
-        for (Student student : students) {
-            if (student.getId() == id) {
-                return student;
-            }
-        }
-
-        return null;
+        return studentRepo.findById(id).orElse(null);
     }
 
     public Student update(int id, Student updatedStudent) {
-        for (Student student : students) {
-            if (student.getId() == id) {
-                student.setName(updatedStudent.getName());
-                student.setEmail(updatedStudent.getEmail());
-                student.setAge(updatedStudent.getAge());
+        Student st = studentRepo.findById(id).orElse(null);
+        if (st != null) {
+            st.setName(updatedStudent.getName());
+            st.setAge(updatedStudent.getAge());
+            st.setEmail(updatedStudent.getEmail());
 
-                return student;
-            }
+            return studentRepo.save(st);
         }
-
         return null;
     }
 
     public String delete(int id) {
-        for (Student student : students) {
-            if (student.getId() == id) {
-                students.remove(student);
-                return "Student Deleted Successfully";
-            }
-        }
-
-        return "Student Not Found";
+        studentRepo.deleteById(id);
+        return "Student Deleted";
     }
 }
