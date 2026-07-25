82ff6959d78a2e2cdf940c55449f61af7138c076
Vinaya <vinayaa.busam18@gmail.com>
2026-07-25 15:32:52 +0000

    Add ResposeEntity Support

--- src/main/java/com/example/student_management_system/controller/StudentController.java
+++ src/main/java/com/example/student_management_system/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.example.student_management_system.controller;
 import com.example.student_management_system.model.Student;
 import com.example.student_management_system.service.StudentService;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import jakarta.validation.Valid;
@@ -23,14 +25,14 @@ public class StudentController {
     }
 
     @PostMapping("/add")
-    public Student add(@Valid @RequestBody Student student) {
-        return studentService.addStudent(student); 
-
+    public ResponseEntity<Student> add(@Valid @RequestBody Student student) {
+        Student addedStudent = studentService.addStudent(student);
+        return new ResponseEntity<>(addedStudent, HttpStatus.CREATED);
     }
 
     @GetMapping("/getAllStudents")
-    public List<Student> getAllStudents() {
-        return studentService.getAllStudents();
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
     }
 
     @GetMapping("/getStudent/{id}")
@@ -39,13 +41,15 @@ public class StudentController {
     }
 
     @PutMapping("/update/{id}")
-    public Student update(@PathVariable int id, @RequestBody Student updatedStudent) {
-        return studentService.update(id, updatedStudent);
+    public ResponseEntity<Student> update(@PathVariable int id, @RequestBody Student updatedStudent) {
+        Student updated = studentService.update(id, updatedStudent);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public String delete(@PathVariable int id) {
-        return studentService.delete(id);
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        studentService.delete(id);
+        return new ResponseEntity<>("Student deleted successfully", HttpStatus.OK);
     }
 
 }
