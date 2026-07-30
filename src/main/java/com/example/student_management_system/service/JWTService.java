a47983120b578b730b9cd3f8a0d94116ac638826
Vinaya <vinayaa.busam18@gmail.com>
2026-07-30 09:54:21 +0000

    Implement JWT Service

--- src/main/java/com/example/student_management_system/service/JWTService.java
+++ src/main/java/com/example/student_management_system/service/JWTService.java
@@ -11,7 +11,8 @@ import java.nio.charset.StandardCharsets;
 import org.springframework.beans.factory.annotation.Value;
 import java.util.Date;
 import org.springframework.security.core.userdetails.UserDetails;
-
+import io.jsonwebtoken.Claims;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
@@ -30,5 +31,40 @@ public class JWTService {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 minutes
                 .signWith(getSigningKey())
                 .compact();
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+            .verifyWith(getSigningKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+
+        Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
     }
 }
